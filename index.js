73c537bac2fc3d8a02fb20472cede49042b85228
emerson souza <emerson-santos1940@hotmail.com>
2024-04-16 21:07:23 -0300

    Projeto Metricas
    Version 1.0

---
+++ index.js
@@ -0,0 +1,11 @@
+let click = document.getElementById("btn");
+let open = document.getElementsByClassName('btn');
+
+function btn1() {
+    window.open('metrics/I.M.C/I.M.C.html', '_Self');
+    Window.close();
+}
+function btn2() {
+    window.open('metrics/C.A/C.A.html', '_Self');
+    window.close();
+}
