2699785dd48b8adc3747031b0f6f49d7d2730ae0
emerson souza <emerson-santos1940@hotmail.com>
2024-04-16 21:17:18 -0300

    Update index.js
    Remove items in functions

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ let open = document.getElementsByClassName('btn');
 
 function btn1() {
     window.open('metrics/I.M.C/I.M.C.html', '_Self');
-    Window.close();
 }
 function btn2() {
     window.open('metrics/C.A/C.A.html', '_Self');
-    window.close();
 }
